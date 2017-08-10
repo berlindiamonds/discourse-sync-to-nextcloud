694da5698c82ea72d0f2cb40256262a2c904e195
KAJATIGER <ksantro@gmx.de>
2017-08-10 11:44:03 +0200

    authorization works a bit different for nextcloud with the ocman gem. there is a config file that does the login I think. so there is no api-key. also I tried to memoize the Ocman session, but I am not sure if it works like this

--- lib/nextcloud_synchronizer.rb
+++ lib/nextcloud_synchronizer.rb
@@ -3,15 +3,15 @@ module DiscourseBackupToNextcloud
 
     def initialize(backup)
       super(backup)
-      @api_key = SiteSetting.discourse_sync_to_nextcloud_api_key
       @turned_on = SiteSetting.discourse_sync_to_nextcloud_enabled
     end
 
-    def session
+    def ocman
+      ocman ||= Ocman.new
     end
 
     def can_sync?
-      @turned_on && @api_key.present? && backup.present?
+      @turned_on && backup.present?
     end
 
     protected
@@ -20,22 +20,22 @@ module DiscourseBackupToNextcloud
       folder_name = Discourse.current_hostname
       full_path = backup.path
       filename = backup.filename
-      file = Ocman.put('#{full_path}', '/#{filename}')
+      file = ocman.put('#{full_path}', '/#{filename}')
       add_to_folder(file, folder_name)
     end
 
     def add_to_folder(file, folder_name)
-      folder = Ocman.list('/#{folder_name}')
+      folder = ocman.list('/#{folder_name}')
       if folder.present?
-        Ocman.move('source_path/of/file', 'destination_path')
+        ocman.move('source_path/of/file', 'destination_path')
       else
-        folder = Ocman.create_folder('/#{folder_name}')
-        Ocman.move('source_path/of/file', 'destination_path')
+        folder = ocman.create_folder('/#{folder_name}')
+        ocman.move('source_path/of/file', 'destination_path')
       end
     end
 
     def remove_old_files(file, folder_name)
-      next_files = Ocman.list('/#{folder_name}/#{files}')
+      next_files = ocman.list('/#{folder_name}/#{files}')
       sorted = next_files.sort_by {|x| x.created_time}
       keep = sorted.take(SiteSetting.discourse_sync_to_nextcloud_quantity)
       trash = next_files - keep
