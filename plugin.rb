aaaea0c277cae130669bf0c3aafaf3f2050e709e
misaka4e21 <misaka4e21@gmail.com>
2018-06-01 14:01:18 +0800

    Update 'multi_json' to '1.13.1'; Improve README.md

--- plugin.rb
+++ plugin.rb
@@ -8,7 +8,7 @@
 gem 'domain_name', '0.5.20170404', { require: false }
 gem 'http-cookie', '1.0.3', { require: false }
 gem 'hashie', '3.5.5', { require: false }
-gem 'multi_json', '1.12.1', { require: false }
+gem 'multi_json', '1.13.1', { require: false }
 gem 'net_dav', '0.5.1', { require: false }
 gem 'netrc', '0.8.0', { require: false }
 gem 'rest-client', '2.0.2', { require: false }
